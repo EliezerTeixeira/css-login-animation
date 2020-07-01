0676cd72f308dc9b187142157eb167cef5f06ba1
EliezerTeixeira <eliezerteixeira@outlook.pt>
2020-07-01 17:40:20 +0100

    vibrate to empty fields

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ const form = document.querySelector('form')
 btnLogin.addEventListener('click', event => {
     event.preventDefault()
 
-    form.classList.add('form-hide')
+    const fields = [...document.querySelectorAll('.input-block input')]
+
+    fields.forEach(field =>{
+        if(field.value === ''){
+            form.classList.add('validate-error')
+        }
+    })
+
+    const formError = document.querySelector('.validate-error')
+
+    if(formError){
+        formError.addEventListener('animationend', event => {
+            if(event.animationName === 'vibrate'){
+                formError.classList.remove('validate-error')
+            }
+        })
+    }else{
+
+        form.classList.add('form-hide')
+    }
+
 })
 
 form.addEventListener('animationstart', event => {
